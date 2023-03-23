51f68341c03e78cf4099836ab3f782252bb25bf4
IUT BLAGNAC <noreply@univ-tlse2.fr>
2023-03-23 15:39:37 +0100

    final commit

--- tests/RadiatorTest.php
+++ tests/RadiatorTest.php
@@ -5,20 +5,32 @@ namespace Tests;
 use PHPUnit\Framework\TestCase;
 use iut\Radiator;
 use iut\Thermometer;
+use iut\iThermometer;
 
-use \Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+
+/* php ./vendor/phpunit/phpunit/phpunit tests/RadiatorTest.php
+
+Commande pour lancer les tests (ne fonctionne pas chez moi sans le 'php' devant) 
+
+*/
 
 require __DIR__ . '/../vendor/autoload.php';
 
-class RadiatorTest extends MockeryTestCase
-{
-    public function testUpdateThermostat()
-    {
-        $radiator = new Radiator(new Thermometer());
+class RadiatorTest extends MockeryTestCase {
+
+    public function testUpdateThermostat() {
+        
+        $mock = \Mockery::mock('Thermometer', 'iThermometer');
+
+        $radiator = new Radiator($mock);
+
+        $mock->shouldReceive('outsideTemperature')->andReturn(3);
 
         $radiator->updateThermostatFromOutsideTemperature();
 
         $this->assertSame(3, $radiator->thermostat);
+
     }
 
 }
